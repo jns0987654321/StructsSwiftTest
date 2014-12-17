04a9c22a06f5d3ea0fc1f58242e023b081da5822
Junior Samaroo <jns0987654321@gmail.com>
2014-12-17 17:03:34 -0400

    Created new struct

---
+++ LionsAndTigers/Tiger.swift
@@ -0,0 +1,13 @@
+//
+//  Tiger.swift
+//  LionsAndTigers
+//
+//  Created by Junior Samaroo on 2014-12-17.
+//  Copyright (c) 2014 Junior Samaroo. All rights reserved.
+//
+
+import Foundation
+
+struct Tiger{
+    
+}
